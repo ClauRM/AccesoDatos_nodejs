8784318ad2ed12546a1d183c9ca7818d1b986e2c
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2024-02-14 18:06:27 +0100

    Guardar información de registros en txt: fecha, hora y url visitada

---
+++ 002-Nodejs/002-ServidorWeb/007-WebRegistros/001-servidor.js
@@ -0,0 +1,51 @@
+var servidor = require('http');//require permite importar modulos
+var archivo = require('fs');//libreria para leer archivos
+
+//crear servidor web haciendo require y response
+servidor.createServer(function (req, res) {
+    //codigo 200 status OK
+    res.writeHead(200, { 'Content-Type': 'text/html' });//tipo de contenido a devolver
+
+    //__dirname asegura que la ruta del archivo es correcta
+    const rutaIndex = __dirname + "/index.html";
+    const rutaAboutme = __dirname + "/aboutme.html";
+    const rutaContact = __dirname + "/contact.html";
+
+    //enrutador
+    //con switch devolvemos mensaje en funcion de lo leido en url
+    switch (req.url) {
+        case "/":
+            console
+            archivo.readFile(rutaIndex, function (err, data) {
+                res.write(data);
+                res.end("");
+            });
+            break;
+        case "/aboutme":
+            archivo.readFile(rutaAboutme, function (err, data) {
+                res.write(data);
+                res.end("");
+            });;
+            break;
+        case "/contact":
+            archivo.readFile(rutaContact, function (err, data) {
+                res.write(data);
+                res.end("");
+            });
+            break;
+        default:
+            res.end("Pagina no encontrada");
+            break;
+    };
+
+    //registro de la url visitada por el usuario
+    if (req.url != "/favicon.ico") {
+        var fecha = new Date();
+        //con req.url almacenamos la pagina visitada
+        //podemos concatenar con la fecha
+        archivo.appendFile(__dirname + '/registro.txt', fecha.getDate() + "-" + (fecha.getMonth() + 1) + "-" + fecha.getFullYear() + " " + fecha.getHours() + ":" + fecha.getMinutes() + ":" + fecha.getSeconds() + " " + req.url + "\n", function (err) {
+            if (err) throw err;
+        });
+    }
+
+}).listen(8080) //puerto 8080
