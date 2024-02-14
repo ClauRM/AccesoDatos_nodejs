c1e3071236d34d43940afebd68823091e1e69b03
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2024-02-14 18:09:10 +0100

    Separación del header y footer en HTML

---
+++ 002-Nodejs/002-ServidorWeb/010-WebHeaderFooterHtml/001-servidor.js
@@ -0,0 +1,80 @@
+var servidor = require('http');//require permite importar modulos
+var archivo = require('fs');//libreria para leer archivos
+var procesador = require('querystring');//utilidades para trabajar con cadenas
+
+//crear servidor web haciendo require y response
+servidor.createServer(function (req, res) {
+    //codigo 200 status OK
+    res.writeHead(200, { 'Content-Type': 'text/html' });//tipo de contenido a devolver
+
+    //__dirname asegura que la ruta del archivo es correcta
+    const rutaCabecera = __dirname + "/header.html";
+    const rutaPiepagina = __dirname + "/footer.html";
+    const rutaIndex = __dirname + "/index.html";
+    const rutaAboutme = __dirname + "/aboutme.html";
+    const rutaContact = __dirname + "/contact.html";
+    const rutaBlog = __dirname + "/blog.html";
+
+    //cargar los html
+    archivo.readFile(rutaCabecera, function (err, data) {
+        res.write(data); //mostrar la cabecera
+
+        //enrutador
+        //con switch devolvemos mensaje en funcion de lo leido en url
+        switch (req.url) {
+            case "/":
+                console
+                archivo.readFile(rutaIndex, function (err, data) {
+                    res.write(data);
+                });
+                break;
+            case "/aboutme":
+                archivo.readFile(rutaAboutme, function (err, data) {
+                    res.write(data);
+                });;
+                break;
+            case "/contact":
+                archivo.readFile(rutaContact, function (err, data) {
+                    res.write(data);
+                });
+                break;
+            case "/blog":
+                archivo.readFile(rutaBlog, function (err, data) {
+                    res.write(data);
+                });
+                break;
+            case "/procesa":
+                res.write("Datos procesados");
+                let datos = "";
+                //listener del lado del servidor
+                req.on("data", parte => {
+                    datos += parte.toString(); //concatenar datos recibidos
+                });
+                req.on("end", () => {
+                    var cadenaProcesada = procesador.parse(datos); //procesar cadena de dato como objeto
+                    console.log(cadenaProcesada);
+                });
+                break;
+            default:
+                res.end("Pagina no encontrada");
+                break;
+        };
+
+        //pie de pagina html
+        archivo.readFile(rutaPiepagina, function (err, data) {
+            res.write(data);
+            res.end(""); //al cargar el pie de pagina res.end
+        });
+    });
+
+    //registro de la url visitada por el usuario
+    if (req.url != "/favicon.ico") {
+        var fecha = new Date();
+        //con req.url almacenamos la pagina visitada
+        //podemos concatenar con la fecha
+        archivo.appendFile(__dirname + '/registro.txt', fecha.getDate() + "-" + (fecha.getMonth() + 1) + "-" + fecha.getFullYear() + " " + fecha.getHours() + ":" + fecha.getMinutes() + ":" + fecha.getSeconds() + " " + req.url + "\n", function (err) {
+            if (err) throw err;
+        });
+    }
+
+}).listen(8080) //puerto 8080
