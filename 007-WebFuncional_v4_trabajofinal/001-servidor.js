1aa1f47a265b8cbf9c8f146482a565ca92691aab
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2024-03-28 14:04:57 +0100

    insertar registro en DB

--- 007-WebFuncional_v4_trabajofinal/001-servidor.js
+++ 007-WebFuncional_v4_trabajofinal/001-servidor.js
@@ -63,8 +63,11 @@ servidor.createServer(function (req, res) {
             case "/":
                 archivo.readFile(rutaIndex, function (err, indexData) {
                     res.write(indexData);
-                    res.end();
                 });
+                archivo.readFile(rutaFooter, function (err, footerData) {
+                    res.write(footerData);
+                    res.end();
+                });                
                 break;
             case "/blog":
                 archivo.readFile(rutaBlog, function (err, blogData) {
@@ -171,19 +174,13 @@ servidor.createServer(function (req, res) {
                 });
                 break;
             case "/registro":
-                res.write(`
-                        <div class="px-5">
-                            <p>Tu tratamiento se ha registrado correctamente.</p> <br>
-                            <p>Vuelve a Inicio para registrar un nuevo tratamiento y a Tratamientos para consultar todos los tratamientos</p>
-                        </div>         
-                         `);
-                let datosRegistro = ""; //var almacenar datos recibidos
-                //listener del lado del servidor
+                let datosRegistro = ""; //variable para almacenar datos recibidos del formulario
+                //listener del lado del servidor para recibir los datos del formulario
                 req.on("data", parte => {
                     datosRegistro += parte.toString(); //concatenar datos recibidos clave=valor
                 });
                 req.on("end", () => {
-                    var cadenaProcesada = procesador.parse(datos); //procesador querystring de cadena de dato como objeto
+                    var cadenaProcesada = procesador.parse(datosRegistro); //procesar la cadena de datos como un objeto
                     //capturar datos en variables
                     var nmedicamento = cadenaProcesada.medicamento;
                     var nfecha = cadenaProcesada.fecha;
@@ -192,7 +189,7 @@ servidor.createServer(function (req, res) {
 
                     //escribir la sentencia SQL
                     const sql = `
-                            INSERT INTO tratamientos (campo1, campo2, campo3, campo4)
+                            INSERT INTO tratamientos (idtratamiento, medicamento, fecha, hora, comentarios)
                             VALUES (NULL, ?, ?, ?, ?)
                         `;
 
@@ -200,38 +197,45 @@ servidor.createServer(function (req, res) {
                     conexion.query(sql, [nmedicamento, nfecha, nhora, ncomentarios], function (error, results, fields) {
                         if (error) {
                             console.error(error.message);
+                            res.write(`
+                            <div class="px-5">
+                                <p>Ha ocorrido un error.
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">
+                                        <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z"/>
+                                        <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+                                    </svg>
+                                </p>
+                            </div>         
+                             `);
                         } else {
                             console.log("Registro de tratamiento insertado correctamente en la BD");
+                            res.write(`
+                            <div class="px-5">
+                                <p>Tu tratamiento se ha registrado correctamente.</p>
+                                <p>Vuelve a Inicio para registrar un nuevo tratamiento y a Tratamientos para consultar todos los tratamientos</p>
+                            </div>         
+                             `);
                         }
+                        archivo.readFile(rutaFooter, function (err, footerData) {
+                            res.write(footerData);
+                            res.end();
+                        });   
                     });
-                    //     //crear nuevo Formulario con los datos a insertar
-                    //     var nuevoFormulario = new Formulario(
-                    //         {
-                    //             nombre: nnombre,
-                    //             fecha: nfecha,
-                    //             email: nemail,
-                    //             comentarios: ncomentarios
-                    //         }
-                    //     );
-                    //     //ejecutar sentencia insertar Formulario
-                    //     nuevoFormulario.save()
-                    //         .then(function () {//despues de ejecutar hacer...
-                    //             console.log("Formulario insertado correctamente");
-                    //         })
-                    // });
-                    break;
+                });
+
+                break;
             default:
                 res.end("Pagina no encontrada");
                 break;
         };
 
-        //pie de pagina html
+        // //pie de pagina html
         // archivo.readFile(rutaFooter, function (err, footerData) {
         //     if (err) {
         //         throw err;
         //     }
         //     res.write(footerData);
-        //     // res.end(); // Finalizar la respuesta después de enviar el pie de página
+        //     res.end(); // Finalizar la respuesta después de enviar el pie de página
         // });
 
     });
